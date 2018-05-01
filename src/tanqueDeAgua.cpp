342cef8bc5cea51084e4776e05e22d65899ac377
Alvaro Iribarren <alvaro_iribarren@hotmail.com>
2018-05-01 05:29:53 +0000

    tanqueDeAgua.cpp edited online with Bitbucket

--- src/tanqueDeAgua.cpp
+++ src/tanqueDeAgua.cpp
@@ -1,36 +1,45 @@
-#include "tanqueDeAgua.h"
+#include "TanqueDeAgua.h"
 
 using namespace std;
 
-tanqueDeAgua::tanqueDeAgua(unsigned int N, unsigned int M, unsigned int dificultad) {
+TanqueDeAgua::TanqueDeAgua(unsigned int N, unsigned int M, unsigned int dificultad) {
 	this->cantidadDeAguaActual=0;
 	this->cantidadDeAguaMaxima= (N * M * 2) / dificultad;
 
 }
 
-void tanqueDeAgua::agregarAgua(int cantidad){
+void TanqueDeAgua::agregarAgua(int cantidad){
 	this->cantidadDeAguaActual += cantidad;
 }
 
-void tanqueDeAgua::restarAgua(int cantidad){
+void TanqueDeAgua::restarAgua(int cantidad){
 	this->cantidadDeAguaActual -= cantidad;
 }
 
-void tanqueDeAgua::ampliarTanque(int dificultad){
+int TanqueDeAgua::obtenerCantidadDeAguaActual(){
+	return this->cantidadDeAguaActual;
+}
+
+int TanqueDeAgua::obtenerCantidadDeAguaMaxima(){
+	return this->cantidadDeAguaMaxima;
+}
+
+void TanqueDeAgua::ampliarTanque(int dificultad, Monedero &monedero){
 	int ampliacionElegida;
 
 	cout << "Puede ampliar el tanque en 5, 10 o 15 unidades,"
 				" para ello presione 1, 2 o 3 respectivamente." << endl;
 	cin >> ampliacionElegida;
+
 	// validar (ampliacionElegida); FALTA CREAR
 
-	cantidadDeAguaMaxima+= ampliacionElegida;  //amplia el tanque
+	cantidadDeAguaMaxima+= ampliacionElegida*5;  //amplia el tanque
 
 	int dineroGastado= ampliacionElegida*5*dificultad;
-	//monedero.restarDinero(dineroGastado);  FALTA CREAR
+	monedero.gastarDinero(dineroGastado);
 
 }
 
-tanqueDeAgua::~tanqueDeAgua() {
+TanqueDeAgua::~TanqueDeAgua() {
 
 }
