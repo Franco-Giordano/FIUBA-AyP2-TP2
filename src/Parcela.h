116ebcbd663126402b17a85ec7809376e66cd689
Alvaro Iribarren <alvaro_iribarren@hotmail.com>
2018-05-09 23:56:33 +0000

    Agregado el cultivar, y un par de funciones que podrían ser útiles.

--- src/Parcela.h
+++ src/Parcela.h
@@ -38,20 +38,40 @@ public:
 	//Post: Crea una parcela, con un tipo de cultivo Vacío, con estado Libre, y sin regar.
 	Parcela();
 
+	
+	//Post: Devuelve true si est� plantada, false si no lo est�.
+	//Podria servir para las validaciones, como estaLibre.
+	bool estaPlantada();
+
+	//Post: Devuelve true si est� regada, false si no lo est�.
+	// Cambie el nombre, porque me parece mas claro comparar con un si esta o no esta regada
+	// que con obtenerRegado.
+	bool estaRegada();
+
+	//Post: Devuelve true si est� libre, false si no lo est�.
+	bool estaLibre();
+
+
+
 	//Post: Devuelve el objeto cultivo por referencia, para modificaciones. OJO CON ESTO, fijense que les parece.
 	Cultivo& obtenerCultivo();
 
+	void liberarParcela();
+
 
 	//Post: Siembra un cultivo, asignandole las propiedades de dicho cultivo al cultivo contenido por la parcela.
-	void sembrarCultivo(std::string nombreLeido,int costoSemillaLeido,int tiempoCosechaLeido,int rentabilidadLeida,
-												int tiempoRecuperacionLeido,int consumoAguaLeido, Monedero &monedero);
+	void sembrarCultivo(std::string nombreLeido,int costoSemillaLeido,int tiempoCosechaLeido,
+						int rentabilidadLeida, int tiempoRecuperacionLeido,Monedero &monedero,
+						int consumoDeAguaRecibido);
 
 	//Pre:  El estado de regada debe ser False.
 	//Post: Cambia el estado de regada a True.
 	void regarParcela(TanqueDeAgua& tanqueAgua,int costoDeAgua);
 
-	//Post: Devuelve true si est� regada, false si no lo est�.
-	bool obtenerRegado();
+	//Agregado que faltaba de antes.
+	void cultivarParcela(Monedero& monedero);
+
+
 
 };
 
