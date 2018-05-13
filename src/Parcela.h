d9f31f95ebdc579404b13fdbd2f11cf2f31134b4
Alvaro Iribarren <alvaro_iribarren@hotmail.com>
2018-05-13 21:19:49 +0000

    Implementado totalmente con punteros (removido el uso de pasaje por referencia de objetos)

--- src/Parcela.h
+++ src/Parcela.h
@@ -11,6 +11,7 @@
 #include "Cultivo.h"
 #include "Monedero.h"
 #include "TanqueDeAgua.h"
+#include "Almacen.h"
 
 
 enum Estado
@@ -52,26 +53,28 @@ public:
 	//Post: Devuelve true si esta libre, false si no lo esta.
 	bool estaLibre();
 
-	//Post: Devuelve un puntero a la "bolsa de semillas", especificamente apuntando al cultivo que está plantado en la parcela.
+	//Post: Devuelve un puntero a la "bolsa de semillas", especificamente apuntando al cultivo que estï¿½ plantado en la parcela.
 	Cultivo* obtenerCultivo();
 
 	//Post:  Libera la parcela, dejando al cultivo en NULL y el estado en Libre.
 	void liberarParcela();
 
 	//Post: Siembra un cultivo, asignandole las propiedades de dicho cultivo al cultivo contenido por la parcela.
-	void sembrarCultivo(Cultivo* cultivoParaSembrar,Monedero &monedero);
+	void sembrarCultivo(Cultivo* cultivoParaSembrar, Monedero* monedero);
 
 	//Pre:  El estado de regada debe ser False.
 	//Post: Cambia el estado de regada a True.
-	void regarParcela(TanqueDeAgua& tanqueAgua,int costoDeAgua);
+	void regarParcela(TanqueDeAgua* tanqueAgua,int costoDeAgua);
 
-	//Agregado que faltaba de antes.
-	void cultivarParcela(Monedero& monedero);
+
+	//Post: quita el cultivo de la parcela y lo manda al almacen.  Luego la parcela es puesta en estado "Plantada"
+	// y su puntero a cultivo es puesto en "Null".
+	void cosecharParcela(Monedero* monedero, Almacen* almacen);
 
 	//Post: Devuelve el tiempo actual que falta hasta la cosecha de la parcela.
 	int obtenerTiempoCosecha();
 
-	//Devuelve el tiempo de recuperación actual de la parcela.
+	//Devuelve el tiempo de recuperaciï¿½n actual de la parcela.
 	int obtenerTiempoRecuperacion();
 
 };
