44fa34eb2db7a2b8eca6edb505372bbc6d8ed3f6
Alvaro Iribarren <alvaro_iribarren@hotmail.com>
2018-05-19 23:35:20 +0000

    ImplementacionDeJugadores.cpp (implementacion actualizada)

--- src/ImplementacionDeJugadores.cpp
+++ src/ImplementacionDeJugadores.cpp
@@ -1,7 +1,15 @@
 
+/* 
+*principal.cpp
+* Created by:Alvaro
+*28 abr. 2018
+*20:27:02
+*/
+
+
 #include <iostream>
 #include "Jugador.h"
-#include "Jugadores.h"
+
 
 using namespace std;
 
@@ -11,51 +19,61 @@ int main ()
 	cout << "Cuantos jugadores van a jugar ? " << endl;
 	cin >> numeroDeJugadores;
 
-	Jugadores jugadores(numeroDeJugadores);
+	int N=5;
+	int M=6;
+	int dificultad=1;
 
-	cout << "Jugadores:" << jugadores.obtenerListaJugadores()->contarElementos() << endl;
+	Lista<Jugador*>* jugadores= new Lista<Jugador*>;
+
+	for (unsigned int i=1; i<= numeroDeJugadores; i++){
+		Jugador* jugador= new Jugador(N, M,dificultad);
+		jugadores->agregar(jugador);
+
+	}
+
+	cout << "Jugadores:" << jugadores->contarElementos() << endl;
 
 	Cultivo* c1= new Cultivo();
 	c1->modificarCultivo("Zanahoria",2,2,2,2,2);
 	Cultivo* c2= new Cultivo();
 	c2->modificarCultivo("Papa", 3,4,3,3,3);
 
-	jugadores.obtenerListaJugadores()->obtener(1)->sembrar(1, 3, 4, c1);
-	jugadores.obtenerListaJugadores()->obtener(1)->sembrar(1, 3, 3, c2);
+	jugadores->obtener(1)->sembrar(1, 3, 4, c1);
+	jugadores->obtener(1)->sembrar(1, 3, 3, c2);
 
-	cout << "Nombre: " << jugadores.obtenerListaJugadores()->obtener(1)->obtenerListaTerreno()->obtener(1)->
+	cout << "Nombre: " << jugadores->obtener(1)->obtenerListaTerreno()->obtener(1)->
 			obtenerParcela(3,4).obtenerCultivo()->obtenerNombre() << endl;
 
-	cout << "Nombre: " << jugadores.obtenerListaJugadores()->obtener(1)->obtenerListaTerreno()->obtener(1)->
+	cout << "Nombre: " << jugadores->obtener(1)->obtenerListaTerreno()->obtener(1)->
 				obtenerParcela(3,3).obtenerCultivo()->obtenerNombre() << endl;
 
-	cout << "Terrenos antes de comprar: "<< jugadores.obtenerListaJugadores()->obtener(1)->
+	cout << "Terrenos antes de comprar: "<< jugadores->obtener(1)->
 			obtenerListaTerreno()->contarElementos()<< endl;
 
-	jugadores.obtenerListaJugadores()->obtener(1)->comprarTerreno(5,5);
+	jugadores->obtener(1)->comprarTerreno(5,5);
 
-	cout << "Terrenos despues de comprar: "<< jugadores.obtenerListaJugadores()->obtener(1)->
+	cout << "Terrenos despues de comprar: "<< jugadores->obtener(1)->
 			obtenerListaTerreno()->contarElementos()<< endl;
 
-	jugadores.obtenerListaJugadores()->obtener(1)->sembrar(2, 3, 2, c1);
+	jugadores->obtener(1)->sembrar(2, 3, 2, c1);
 
-	cout << "Nombre: " << jugadores.obtenerListaJugadores()->obtener(1)->obtenerListaTerreno()->obtener(2)->
+	cout << "Nombre: " << jugadores->obtener(1)->obtenerListaTerreno()->obtener(2)->
 					obtenerParcela(3,2).obtenerCultivo()->obtenerNombre() << endl;
 
 
-	jugadores.obtenerListaJugadores()->obtener(1)->venderTerreno(2);
+	jugadores->obtener(1)->venderTerreno(2);
 
-	cout << "Terrenos despues de vender: "<< jugadores.obtenerListaJugadores()->obtener(1)->
+	cout << "Terrenos despues de vender: "<< jugadores->obtener(1)->
 				obtenerListaTerreno()->contarElementos()<< endl;
 
-	int dificultad=2;
 
-	cout << "Capacidad del almacen: " << jugadores.obtenerListaJugadores()->obtener(1)->obtenerAlmacen()->
+
+	cout << "Capacidad del almacen: " << jugadores->obtener(1)->obtenerAlmacen()->
 			obtenerCapacidad()<< endl;
 
-	jugadores.obtenerListaJugadores()->obtener(1)->agrandarAlmacen(5, dificultad);
+	jugadores->obtener(1)->agrandarAlmacen(5, dificultad);
 
-	cout << "Capacidad del almacen: " << jugadores.obtenerListaJugadores()->obtener(1)->obtenerAlmacen()->
+	cout << "Capacidad del almacen: " << jugadores->obtener(1)->obtenerAlmacen()->
 			obtenerCapacidad()<< endl;
 
 
@@ -65,3 +83,4 @@ int main ()
 
 return 0;
 }
+
