05bd5ac69d678dde02c5f8f94b3315d1d1e3bec6
franco-giordano <francogior98@gmail.com>
2018-05-20 20:21:11 -0300

    Primeros pasos del main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,48 @@
  *      Author: frank
  */
 
+#include "LectorCSV.h"
+#include "Catalogo.h"
 
+int main () {
+
+	LectorCSV lectorSemillas("cultivos.txt", 6);
+
+	LectorCSV lectorDestinos("destinos.txt", 4);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
 
 
