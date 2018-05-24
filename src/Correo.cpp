649036a5de6f410e4927e8ef021a8654a947f4a9
Ailu García <ailu.tunci@gmail.com>
2018-05-24 22:17:38 +0000

    Correo.cpp edited online with Bitbucket

--- src/Correo.cpp
+++ src/Correo.cpp
@@ -2,9 +2,9 @@
 #include <iostream>
 using namespace std;
 
-Correo::Correo(CatalogoDe<Destino*>* destinos, Cultivo* cultivoAEnviar)
+Correo::Correo(Lista<Destino*>* destinosValidos, Cultivo* cultivoAEnviar)
 {
-	this->destinos=destinos;
+	this->destinosValidos=destinosValidos;
 	this->costoEnvio=0;
 	this->cultivoAEnviar=cultivoAEnviar;
 }
@@ -14,41 +14,24 @@ Cultivo* Correo::cultivoParaEnviar()
 	return cultivoAEnviar;
 }
 
-void Correo::enviarCultivo(Almacen* almacen)
+void Correo::enviarCultivo(unsigned int posicionCultivoAEnviar, Almacen* almacen)
 {
-	for(unsigned int i=1; i<=almacen->cultivos->contarElementos(); i++)
-	{
-		if(almacen->cultivos->obtener(i) == cultivoAEnviar)
-			almacen->quitarCultivo(i);
-	}
+	almacen->quitarCultivo(posicionCultivoAEnviar);
 }
 
-void Correo::cobrar(Almacen* almacen, Destino* destino, Monedero* monedero)
+void Correo::cobrar(Destino* destino, Monedero* monedero)
 {
-	unsigned int rentabilidad=obtenerRentabilidad(almacen, destino);
-	int ganancia=costoEnvio+rentabilidad;
+	unsigned int rentabilidad=obtenerRentabilidad();
+	int ganancia=calcularCostoEnvio(destino)+rentabilidad;
 	monedero->sumarDinero(ganancia);
 }
 
-unsigned int Correo::obtenerRentabilidad(Almacen* almacen, Destino* destino)
+unsigned int Correo::obtenerRentabilidad()
 {
-	unsigned int rentabilidad=0;
-	costoEnvio=calcularCostoEnvio(destino);
-	for(unsigned int i=1; i<=almacen->cultivos->contarElementos(); i++)
-	{
-		if(cultivoAEnviar==almacen->cultivos->obtener(i))
-			rentabilidad=cultivoAEnviar->obtenerRentabilidad();
-	}
-	return rentabilidad;
+	return cultivoAEnviar->obtenerRentabilidad();
 }
 
 int Correo::calcularCostoEnvio(Destino* destino)
 {
-	int costoEnvio=0;
-	for(unsigned int i=1; i<=destinos->obtenerCantidadDisponible(); i++)
-	{
-		if(destino==destinos->obtenerPosicion(i))
-			costoEnvio=(destino->obtenerPrecio()*destino->obtenerDistancia());
-	}
-	return costoEnvio;
+	return destino->obtenerPrecio()*destino->obtenerDistancia();
 }
