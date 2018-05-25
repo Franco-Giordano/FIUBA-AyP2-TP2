fbbd3d8b9cd852cb53266af17a5635d84ff1d933
AlvaroIribarren <alvaro_iribarren@hotmail.com>
2018-05-25 02:35:32 -0300

    Agregadas pre y post condiciones

--- src/Mercado.h
+++ src/Mercado.h
@@ -1,28 +1,23 @@
-/*
- * Mercado.h
- *
- *  Created on: 23 may. 2018
- *      Author: frank
- */
-
-#ifndef MERCADO_H_
-#define MERCADO_H_
-
-#include "Jugador.h"
-
 class Mercado{
 private:
 	unsigned int dificultad;
 public:
-
+	//Pre: recibe la dificultad del juego
+	//Post: crea un objeto mercado, el cual se encarga de manejar las compras del jugador
 	Mercado (unsigned int dificultad);
 
-	void comprarCapacidadTanque(Jugador* jugador, unsigned int ampliacionElegida);
+	//Pre: recibe el jugador que va a realizar la accion y la ampliacion que se va a realizar
+	//Post: el mercado le vende al jugador la capacidad del tanque recibida.
+	void venderCapacidadDeTanque(Jugador* jugador, unsigned int ampliacionElegida);
 
-	void comprarCapacidadAlmacen(Jugador* jugador, unsigned int ampliacionElegida);
+	//Pre: recibe el jugador que va a realizar la accion y la ampliacion que se va a realizar
+	//Post: el mercado le vende al jugador la capacidad del almacen recibida.
+	void venderCapacidadDeAlmacen(Jugador* jugador, unsigned int ampliacionElegida);
 
-	void venderTerrenoAjugador(Jugador* jugador, unsigned int filas, unsigned int columnas) ;
+	//Post: El mercado le vende al jugador un terreno, agregandolo a su lista de terrenos.
+	void venderTerrenoAJugador(Jugador* jugador, unsigned int filas, unsigned int columnas) ;
 
+	//Post: remueve de la lista de terrenos un terreno, sumando el dinero al jugador.
 	void comprarTerrenoDeJugador(Jugador* jugador, unsigned int posicionTerreno, unsigned int costoActual);
 
 };
