b154cf1a30606f2c8dc331066ba38b8fa264ba10
Franco Giordano <francogior98@gmail.com>
2018-05-25 07:27:41 +0000

    Participantes.h

---
+++ src/Participantes.h
@@ -0,0 +1,23 @@
+#ifndef PARTICIPANTES_H_
+#define PARTICIPANTES_H_
+
+#include "Jugador.h"
+
+class Participantes {
+
+private:
+	Lista<Jugador*>* participantes;
+
+public:
+	//Pre: numeroDeJugadores es un entero valido ingresado por el usuario
+	//Post: se crea la lista de jugadores.
+
+	Participantes(unsigned int numeroDeJugadores, int M, int N, int dificultad);
+
+	//Post: devuelve la lista de jugadores.
+	Lista <Jugador*>* obtenerListaParticipantes();
+
+	~Participantes();
+};
+
+#endif /* PARTICIPANTES_H_ */
