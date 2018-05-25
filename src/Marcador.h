8c019e907738798ae8aab0e5c7cef578ccf7a1fe
Franco Giordano <francogior98@gmail.com>
2018-05-25 07:03:47 +0000

    Marcador.h terminado

--- src/Marcador.h
+++ src/Marcador.h
@@ -23,13 +23,52 @@ public:
 	//Post: Muestra por pantalla la cantidad de terrenos que posee, su dinero, su uso del tanque de agua y del almacen.
 	void mostrarTurno(Jugador* jugadorActual);
 
-
-	void mostrarPuntuacionFinal() {
-		//FALTA HACERJKFALJSDKLÑFJADFJADFJKLASDFJADFJFJ
-	}
+	//Pre: recibe un puntero a una lista de punteros Jugador. Cada puntero Jugador ya apunta a un jugador previamente creado e inicializado.
+	//Post: imprime por consola la cant de terrenos y creditos de cada jugador, y anuncia el ganador (aquel jugador con mayores creditos)	
+	void mostrarPuntuacionFinal(Lista<Jugador*>* jugadores);
 
 
 };
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * Marcador.h
+ *
+ *  Created on: 23 may. 2018
+ *      Author: frank
+ */
+
+#ifndef MARCADOR_H_
+#define MARCADOR_H_
+
+#include "Jugador.h"
+#include <iostream>
+
+class Marcador {
+private:
+
+public:
+	
+
+
+
+
+
+
+};
+
+
+#endif /* MARCADOR_H_ */
+
 
 
 #endif /* MARCADOR_H_ */
