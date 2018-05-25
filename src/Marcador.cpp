6d324dbb4b8f2e09e6b869dc4dae6bd6c5feaa8d
Franco Giordano <francogior98@gmail.com>
2018-05-25 07:06:24 +0000

    Marcador.cpp terminado xd

--- src/Marcador.cpp
+++ src/Marcador.cpp
@@ -17,10 +17,31 @@ void Marcador::mostrarTurno(Jugador* jugadorActual){
 		Monedero* monedero = jugadorActual->obtenerMonedero();
 		TanqueDeAgua* tanque = jugadorActual->obtenerTanqueDeAgua();
 
+
+
 		std::cout<<"//Cantidad de terrenos propios: "<< jugadorActual->obtenerListaTerreno()->contarElementos()<<std::endl;
 		std::cout<<"//Dinero disponible: "<< monedero->obtenerDineroActual()<<std::endl;
 		std::cout<<"//Uso del tanque de agua: "<< tanque->obtenerCantidadDeAguaActual()<<"/"<<tanque->obtenerCantidadDeAguaMaxima()<<std::endl;
-		std::cout<<"//Uso del almacen: "<<almacen->contarCultivos()<<"/"<<almacen->obtenerCapacidad()<<std::endl<<std::endl;
+		std::cout<<"//Uso del almacen: "<<almacen->contarCultivos()<<"/"<<almacen->obtenerCapacidad()<<std::endl;
 
+}
+
+void Marcador::mostrarPuntuacionFinal(Lista<Jugador*>* jugadores)
+{
+	unsigned int ganador = 1;
+	for (unsigned int j=1; j<=jugadores->contarElementos(); j++)
+	{
+		Jugador* jugadorActual=jugadores->obtener(j);
+
+		std::cout<<"Jugador "<<j<<":"<<std::endl;
+		std::cout<<"	- Cantidad de terrenos: "<<jugadorActual->obtenerListaTerreno()->contarElementos()<<std::endl;
+		std::cout<<"	- Creditos: "<<jugadorActual->obtenerMonedero()->obtenerDineroActual()<<std::endl<<std::endl;
+
+		if(j > 1 && jugadorActual->obtenerMonedero()->obtenerDineroActual() > jugadores->obtener(j-1)->obtenerMonedero()->obtenerDineroActual()) {
+			ganador=j;
+		}
 	}
+
+	std::cout<<std::endl<<"El ganador es el jugador "<< ganador;
+}
 
