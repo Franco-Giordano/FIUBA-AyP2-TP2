6a039b17b0aea7e14fb5d75b4b14076fd14e49f2
Franco Giordano <francogior98@gmail.com>
2018-05-25 07:28:15 +0000

    Participantes.cpp

---
+++ src/Participantes.cpp
@@ -0,0 +1,30 @@
+/*
+ * Jugadores.cpp
+ *
+ *  Created on: 24 may. 2018
+ *      Author: Alvaro
+ */
+
+#include "Participantes.h"
+
+Participantes::Participantes(unsigned int numeroDeJugadores, int N, int M, int dificultad) {
+	this->participantes= new Lista<Jugador*>;
+
+	for (unsigned int i=1; i<= numeroDeJugadores; i++){
+			Jugador* jugador= new Jugador(N, M,dificultad);
+			participantes->agregar(jugador);
+		}
+
+}
+
+Lista<Jugador*>* Participantes::obtenerListaParticipantes(){
+	return this->participantes;
+}
+
+Participantes::~Participantes() {
+	participantes->iniciarCursor();
+	while (participantes->avanzarCursor()) {
+		delete participantes->obtenerCursor();
+	}
+	delete participantes;
+}
