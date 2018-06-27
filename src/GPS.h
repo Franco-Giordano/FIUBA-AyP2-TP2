8c3d7b1c2dc3892d6df8fc99aedb0aa68b3bd892
Ailu García <ailu.tunci@gmail.com>
2018-06-27 03:04:11 +0000

    Separado en .cpp y en .h

--- src/GPS.h
+++ src/GPS.h
@@ -1,12 +1,13 @@
 /*
  * GPS.h
  *
- *  Created on: 25 jun. 2018
- *      Author: franco
+ *  Created on: 26 jun. 2018
+ *      Author: ailen-magali
  */
 
-#ifndef SRC_GPS_H_
-#define SRC_GPS_H_
+#ifndef GPS_H_
+#define GPS_H_
+
 
 #include <string>
 #include "GrafoDirigidoPonderado.h"
@@ -26,131 +27,22 @@ private:
 
 	ListaNombrada<unsigned int>** mejoresCostos;
 
-	unsigned int minimoEntre(unsigned int elemento1, unsigned int elemento2){
+	unsigned int minimoEntre(unsigned int elemento1, unsigned int elemento2);
 
-		unsigned int minimo;
+	unsigned int obtenerPosicionDeNombre(std::string nombre);
 
-		if (elemento1 < elemento2){
-			minimo = elemento1;
-		}
-		else {
-			minimo = elemento2;
-		}
-
-		return minimo;
-	}
-
-	unsigned int obtenerPosicionDeNombre(std::string nombre) {
-		unsigned int i = 0;
-		bool encontrado = false;
-		while (!encontrado && i < catalogoSemillas->obtenerCantidadDisponible()) {
-			encontrado = catalogoSemillas->obtenerPosicion(i)->obtenerNombre() == nombre;
-			i++;
-		}
-		return i-1;
-	}
-
-	void removerInfinitos(ListaNombrada<unsigned int>*& lista) {
-		ListaNombrada<unsigned int>* aux = new ListaNombrada<unsigned int>();
-		lista->iniciarCursor();
-		while (lista->avanzarCursor()) {
-			if (lista->obtenerDatoCursor() < INFINITO) {
-				aux->agregar(lista->obtenerNombreCursor(), lista->obtenerDatoCursor());
-			}
-		}
-
-		delete lista;
-
-		lista = aux;
-	}
+	void removerInfinitos(ListaNombrada<unsigned int>*& lista);
 
 public:
-	GPS(std::string origenRecibido, CatalogoDe<Cultivo>* catalogoSemillas, CatalogoDe<Destino>* catalogoDestinos) {
 
-		this->catalogoSemillas = catalogoSemillas;
+	GPS(std::string origenRecibido, CatalogoDe<Cultivo>* catalogoSemillas, CatalogoDe<Destino>* catalogoDestinos);
 
-		this->cantidadGrafos = catalogoSemillas->obtenerCantidadDisponible();
+	ListaNombrada<unsigned int>* hallarCaminoMinConDijkstra(GrafoDirigidoPonderado<Destino>* grafo);
 
-		this->origen = origenRecibido;
+	ListaNombrada<unsigned int>* obtenerMejoresCostosPara(std::string nombreCultivo);
 
-		this->grafos = new GrafoDirigidoPonderado<Destino>*[cantidadGrafos];
-
-		this->mejoresCostos = new ListaNombrada<unsigned int>*[cantidadGrafos];
-
-		for (unsigned int i = 0; i < cantidadGrafos; i++) {
-			grafos[i] = new GrafoDirigidoPonderado<Destino>(catalogoDestinos,
-															catalogoSemillas->obtenerPosicion(i)->obtenerNombre());
-
-			ListaNombrada<unsigned int>* mejoresCostosCultivoActual = this->hallarCaminoMinConDijkstra(this->grafos[i]); //grafo en la posicion i
-
-			this->removerInfinitos(mejoresCostosCultivoActual);
-
-			mejoresCostos[i] = mejoresCostosCultivoActual;
-		}
-	}
-
-	ListaNombrada<unsigned int>* hallarCaminoMinConDijkstra(GrafoDirigidoPonderado<Destino>* grafo) {
-
-		ListaNombrada<ListaNombrada<unsigned int>*>* listaAdyacentes= grafo->obtenerListaAdyacencia();
-
-		ListaNombrada<unsigned int>* mejoresCaminos;
-
-		if (listaAdyacentes->yaExisteNombre(this->origen)) {
-			unsigned int posOrigen = listaAdyacentes -> obtenerPosicionConNombre(this->origen);
-
-			ColaConPrioridad cola(listaAdyacentes, posOrigen);
-
-			mejoresCaminos = cola.convertirColaAlistaNombrada();
-
-			while (!cola.estaVacia()) {
-
-				Candidato<std::string> raizRemovida = cola.removerRaiz();
-
-				ListaNombrada<unsigned int>* adyacentesActual = listaAdyacentes->obtenerDatoDeNombre(raizRemovida.obtenerIdentificador());
-
-				adyacentesActual->iniciarCursor();
-
-				while (adyacentesActual->avanzarCursor()){
-
-					std::string nombreAdyacenteActual = adyacentesActual->obtenerNombreCursor();
-
-					unsigned int pesoAnterior = mejoresCaminos->obtenerDatoDeNombre(nombreAdyacenteActual);
-
-					unsigned int pesoNuevo = raizRemovida.obtenerPeso() + adyacentesActual->obtenerDatoCursor();
-
-					if (pesoNuevo < pesoAnterior){
-
-						mejoresCaminos->modificarDatoConNombre(nombreAdyacenteActual, pesoNuevo);
-
-						if (cola.estaNombre(nombreAdyacenteActual)) {
-							cola.mejorarPeso(nombreAdyacenteActual, pesoNuevo);
-						}
-					}
-				}
-			}
-		}
-		else {
-			mejoresCaminos = new ListaNombrada<unsigned int>();
-		}
-
-		return mejoresCaminos;
-	}
-
-	ListaNombrada<unsigned int>* obtenerMejoresCostosPara(std::string nombreCultivo){
-
-		unsigned int pos = this->obtenerPosicionDeNombre(nombreCultivo);
-
-		return mejoresCostos[pos];
-	}
-
-	~GPS() {
-		for (unsigned int i = 0; i < cantidadGrafos; i++) {
-			delete grafos[i];
-			delete mejoresCostos[i];
-		}
-		delete[] grafos;
-		delete[] mejoresCostos;
-	}
+	~GPS();
 };
 
-#endif /* SRC_GPS_H_ */
+
+#endif /* GPS_H_ */
