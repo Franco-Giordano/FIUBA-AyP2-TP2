2e42c1b37862ef8c7b2caf392ab849a8ff99180c
Ailu García <ailu.tunci@gmail.com>
2018-06-27 02:56:54 +0000

    Arreglo de templates

--- src/Candidato.h
+++ src/Candidato.h
@@ -1,15 +1,9 @@
-/*
- * Candidato.h
- *
- *  Created on: 22 jun. 2018
- *      Author: franco
- */
-
-#ifndef SRC_CANDIDATOH
-#define SRC_CANDIDATOH
+#ifndef CANDIDATO_H_
+#define CANDIDATO_H_
 
 template <class T>
-class Candidato {
+class Candidato{
+
 private:
     T identificador;
 
@@ -19,27 +13,43 @@ public:
 
     //Post: crea un candidato, el cual representa un nodo del heap y que posee un identificador y un peso. Este peso
     // es el del camino del grafo ponderado.
-    Candidato<T>(T ident, unsigned int peso) {
-        this->identificador = ident;
-
-        this->peso = peso;
-    }
+    Candidato<T>(T ident, unsigned int peso);
 
     // Post: devuelve el identificador que es de tipo T.
-    T obtenerIdentificador() {
-        return identificador;
-    }
+    T obtenerIdentificador();
 
     // Post: modifica el peso actual por el nuevoPeso recibidio por parametro.
-    void modificarPeso(unsigned int nuevoPeso){
-        this->peso= nuevoPeso;
-    }
+    void modificarPeso(unsigned int nuevoPeso);
 
     // Post: devuelve el peso del candidato.
-    unsigned int obtenerPeso() {
-        return peso;
-    }
+    unsigned int obtenerPeso();
 };
 
 
-#endif /* SRC_CANDIDATOH */
+template <class T>
+Candidato<T>::Candidato(T ident, unsigned int peso) {
+
+	this->identificador = ident;
+
+    this->peso = peso;
+}
+
+template <class T>
+T Candidato<T>::obtenerIdentificador() {
+
+	return identificador;
+}
+
+template <class T>
+void Candidato<T>::modificarPeso(unsigned int nuevoPeso){
+
+	this->peso= nuevoPeso;
+}
+
+template <class T>
+unsigned int Candidato<T>::obtenerPeso() {
+
+	return peso;
+}
+
+#endif /* CANDIDATO_H_ */
