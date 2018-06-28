5df9e1dd91be723cf0956f124e191f9ac0e3e3b4
Ailu García <ailu.tunci@gmail.com>
2018-06-28 19:28:52 +0000

    Pre y post

--- src/GPS.h
+++ src/GPS.h
@@ -27,20 +27,47 @@ private:
 
 	ListaNombrada<unsigned int>** mejoresCostos;
 
+	/*
+	* POST: Recibe dos unsigned int y devuelve el menor de ellos.
+	*/
 	unsigned int minimoEntre(unsigned int elemento1, unsigned int elemento2);
-
+	
+	/*
+	* POST: Devuelve la posición en la que se encuentra el Destino de nombre "nombre" en el catálogoSemillas.
+	* Si no lo encuentra, devuelve -1.
+	*/
 	unsigned int obtenerPosicionDeNombre(std::string nombre);
 
+	/*
+	* POST: Guarda en la lista "lista" aquellos datos que tengan un peso menor a INFINITO
+	*/
 	void removerInfinitos(ListaNombrada<unsigned int>*& lista);
 
 public:
 
+	/*
+	* POST: Inicializa catalogoSemillas y origen con los parámetros recibidos, cantidadGrafos
+	* con la cantidad de semillas disponibles en el catálogo, grafos con un array dinámico
+	* de GrafoDirigidoPonderado<Destino> de tamaño cantidadGrafos, mejores costos con una ListaNombrada
+	* de tamaño CantidadGrafos, en donde para cada posicion del array grafos se calculará el camino
+	* mínimo con Dijkstra y se agregará a mejoresCostos en la posición de ese grafo.
+	*/
 	GPS(std::string origenRecibido, CatalogoDe<Cultivo>* catalogoSemillas, CatalogoDe<Destino>* catalogoDestinos);
 
+	/*
+	* POST: Devuelve los caminos de menor costo de grafo calculado con el método de Dijkstra basado en una lista de adyacencias y una cola con
+	* prioridad
+	*/
 	ListaNombrada<unsigned int>* hallarCaminoMinConDijkstra(GrafoDirigidoPonderado<Destino>* grafo);
-
+	
+	/*
+	* POST: Devuelve los mejores costos para un cultivo dado
+	*/
 	ListaNombrada<unsigned int>* obtenerMejoresCostosPara(std::string nombreCultivo);
 
+	/*
+	* POST: Libera la memoria utilizada
+	*/
 	~GPS();
 };
 
